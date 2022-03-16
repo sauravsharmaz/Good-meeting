c16505771fa60e56ec20c9786c95ef7fe59d57d8
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-03-16 10:53:37 +0530

    darkmode modified

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,8 @@
+
+$(document).ready(function(){
+const moon = $(".moon");
+const sun = $(".sun");
+const circle = $(".circle");
 // make headerContainer fixed on scrolling
 window.onscroll =  () => {
     if (window.scrollY > 99) {
@@ -9,33 +14,34 @@ window.onscroll =  () => {
     }
 }
 // dark mode toggle functionality
-const moon = $(".moon");
-const sun = $(".sun");
-const circle = $(".circle");
-circle.click(function() {
-    // circle.animate({right: '3rem'});
-    // document.documentElement.classList.toggle('dark-mode');
-    document.body.style.backgroundColor = "#0E1525";
-    document.querySelector('.headerContainer').style.backgroundColor = "#0E1525";
 
-    $('p').addClass('white');
-    $('h1').addClass('white');
-    $('a').addClass('white');
-    $('.subHeading1').addClass('white');
-    $('#havea-email').addClass('havea-email');
-    $('.meetingLink').addClass('meetingLink-dark');
-    // add white class to every p tag names
-    
-    
-    
-    // document.getElementsByTagNameNS("a").classList.add('white')
-    // document.getElementsByTagNameNS("div").classList.add('white')
-    
-    // document.querySelectorAll('.inverted').forEach(element => {
-    //     element.classList.toggle('invert');
-    // });
-    // document.querySelectorAll(".button1").forEach(element => {
-    //     element.classList.toggle('invert');
-    // });
+$('.darkSwitch').click(function() {
+    circle.toggleClass('hide')
+    moon.toggleClass('hide');
+    if(circle.hasClass('hide')){
+        moon.css("", "yellow")
+        let logoContainerImg = $('.logoContainer img')[0].remove();
+        $(".logoContainer").html(`<img src = "../assets/images/logo2.png"></img>`)
+        document.body.style.backgroundColor = "#0E1525";
+        document.querySelector('.headerContainer').style.backgroundColor = "#0E1525";
+        $('p').addClass('white');
+        $('h1').addClass('white');
+        $('a').addClass('white');
+        $('.subHeading1').addClass('white');
+        $('#havea-email').addClass('havea-email');
+        $('.meetingLink').addClass('meetingLink-dark');
+        
+    }else{
+        let logoContainerImg = $('.logoContainer img')[0].remove();
+        $(".logoContainer").html(`<img src = "../assets/images/logo.png"></img>`)
+        document.body.style.backgroundColor = "white";
+        document.querySelector('.headerContainer').style.backgroundColor = "white";
+        $('p').removeClass('white');
+        $('h1').removeClass('white');
+        $('a').removeClass('white');
+        $('.subHeading1').removeClass('white');
+        $('#havea-email').removeClass('havea-email');
+        $('.meetingLink').removeClass('meetingLink-dark');
+    }
 })
-
+});
